c2940a3651928f5713ca43ec9f29bf5361a79967
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 02:25:32 +0530

    Demo 3 add color to varable

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int phoneNumber= 456789;
+        String areaCode = "(123)";
+        String color = "\033[35;1m", reset = "\\033[30;0m";
+        
+
+
+    }
+}
