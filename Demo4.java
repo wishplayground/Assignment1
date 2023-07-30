b993b53bf03f409e3770bcbebca12e49e7ca3771
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 09:59:11 +0530

    Add same  color

--- Demo4.java
+++ Demo4.java
@@ -1,6 +1,7 @@
 public class Demo4 {
     public static void main(String[] args) {
         int day = 28, month = 12, year = 2023,hour = 23,minutes = 59, second = 59;
-        System.out.printf("");
+        String color = "\033[35;1m", reset = "\033[30;0m";
+        System.out.printf("\033[35;1m%d/\033[35;1m%s/\033[35;1m%s \033[35;1m%s:\033[35;1m%s:\Add033[35;1m%s");
     }
 }
