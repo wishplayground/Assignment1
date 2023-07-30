25f2977ea0ebb5c67e7f36d4ee0ef176bc01c176
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 09:54:11 +0530

    Declare variables

--- Demo4.java
+++ Demo4.java
@@ -1,3 +1,6 @@
 public class Demo4 {
-    
+    public static void main(String[] args) {
+        int day = 28, month = 12, year = 2023,hour = 23,minutes = 59, second = 59;
+        System.out.printf("");
+    }
 }
