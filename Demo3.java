cae9cc46ebb7dabac050b911ddc86169f1f116a2
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 08:51:39 +0530

    Declare variables

--- Demo3.java
+++ Demo3.java
@@ -1,9 +1,9 @@
 public class Demo3 {
     public static void main(String[] args) {
-        int phoneNumber= 456789;
+        int phoneNumber= 4567890;
         String areaCode = "(123)";
         String color = "\033[35;1m", reset = "\\033[30;0m";
-        
+        System.out.printf("%s %s-%s")
 
 
     }
