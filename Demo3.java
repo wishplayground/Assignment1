4c585cb9796d19a4ecdc49aa9e23c356c7ec9606
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 09:46:48 +0530

    print telephone number

--- Demo3.java
+++ Demo3.java
@@ -1,10 +1,9 @@
 public class Demo3 {
     public static void main(String[] args) {
-        int phoneNumber= 4567890;
+        int prefix= 456,suffix = 7890 ;
         String areaCode = "(123)";
-        String color = "\033[35;1m", reset = "\\033[30;0m";
-        System.out.printf("%s %s-%s")
-
+        String color = "\033[35;1m", reset = "\033[30;0m";
+        System.out.printf("%2$s%1$s%3$s %4$d-%5$d\n",areaCode,color,reset,prefix,suffix);
 
     }
 }
