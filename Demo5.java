26cf1eaca93a93c21596b5c88ef0800adb0445ad
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 19:49:10 +0530

    add new line syntax to demo4 and demo5

--- Demo5.java
+++ Demo5.java
@@ -1,6 +1,6 @@
 public class Demo5 {
     public static void main(String[] args) {
         int fNum = 978, sNum = 3, tNum = 16, forthNum = 1484100;
-        
+        System.out.printf("\033[35;1m%d\033[30;0m-\033[31;1m%d\033[31;0m-\033[33;1m%d\033[33;0m-\033[34;1m%d\033[34;0m\n",fNum,sNum,tNum,forthNum);
     }
 }
