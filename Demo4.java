165f5a1cb0d7447d9442f0032d6a801674518d0f
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-07-30 19:37:07 +0530

    unwanted string variables removed

--- Demo4.java
+++ Demo4.java
@@ -1,7 +1,6 @@
 public class Demo4 {
     public static void main(String[] args) {
         int day = 28, month = 12, year = 2023,hour = 23,minutes = 59, second = 59;
-        String color = "\033[35;1m", reset = "\033[30;0m";
         System.out.printf("\033[35;1m%d\033[30;0m/\033[31;1m%s\033[31;0m/\033[33;1m%s\033[33;0m \033[34;1m%s\033[34;0m:\033[36;1m%s\033[36;0m:\033[32;1m%s\033[30;0m", month,day, year,hour,minutes,second);
         
     }
