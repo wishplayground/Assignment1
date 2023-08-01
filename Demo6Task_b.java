62d76ecbbfd21de3e3492f60a51006ba8525298b
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-01 11:33:20 +0530

    started Using repeat sysntax

---
+++ Demo6Task_b.java
@@ -0,0 +1,9 @@
+public class Demo6Task_b {
+    public static void main(String[] args) {
+        System.out.print("+");
+        System.out.print("-".repeat(17));
+        System.out.print("+");
+        System.out.print("-".repeat(5));
+        System.out.print("+");
+    }
+}
