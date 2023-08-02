d490de5750aa783db41f65319f90903326c4bc54
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-02 21:18:54 +0530

    Complete task a

--- Demo6Task_b.java
+++ Demo6Task_b.java
@@ -1,9 +1,11 @@
 public class Demo6Task_b {
     public static void main(String[] args) {
-        System.out.print("+");
-        System.out.print("-".repeat(17));
-        System.out.print("+");
-        System.out.print("-".repeat(5));
-        System.out.print("+");
+        String yellowClr = "\033[0;33m",redClr = "\033[0;31m" ,reset  = "\033[0m";
+        System.out.printf("%s+-----------------+-----+\033[0m%s\n",yellowClr,reset);
+        System.out.printf("%1$s|%2$s \033[1;34m%3$s\033[0m            %1$s|%2$s \033[1;34m%4$s\033[0m %1$s|%2$s\n",yellowClr,reset,"Name","Age");
+        System.out.printf("%s+-----------------+-----+\033[0m%s\n",yellowClr,reset);
+        System.out.printf("%1$s|%3$s %4$s           %1$s|%3$s %2$s%5$d%3$s %1$s |%3$s\n",yellowClr,redClr,reset,"Alice",24);
+        System.out.printf("%1$s|%3$s %4$s             %1$s|%3$s %2$s%5$d%3$s %1$s |%3$s\n",yellowClr,redClr,reset,"Bob",30);
+        System.out.printf("%s+-----------------+-----+\033[0m%s\n",yellowClr,reset);
     }
 }
