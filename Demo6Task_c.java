5049b8925c53a68e59849a1209deaa44fe69e8f6
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-04 07:49:14 +0530

    Completed task c

--- Demo6Task_c.java
+++ Demo6Task_c.java
@@ -1,5 +1,15 @@
 public class Demo6Task_c {
     public static void main(String[] args) {
-        
-    }
+        int pop1 = 3966936, pop2 = 8336817;
+        String yellowClr = "\033[0;33m",redClr = "\033[0;31m" ,reset  = "\033[0m";
+        String boader = String.format("%s+-----------------+--------------+------------+%s",yellowClr,reset);
+        String line = String.format("%1$s|%2$s",yellowClr,reset);
+        System.out.println(boader);
+        System.out.printf("%1$s \033[1;37m%3$s            %1$s \033[1;37m%4$s        %1$s \033[1;37m%5$s %1$s   \033[0m\n",line,reset,"City","State","Population");
+        System.out.println(boader);
+        System.out.printf("%1$s %4$s     %1$s %5$s    %1$s \033[0;31m%6$,d\033[0m  %1$s   \n",line,redClr,reset,"Los Angeles","Califonia",pop1);
+        System.out.printf("%1$s \033[0;32mNew\033[0m %4$s        %1$s \033[0;32mNew\033[0m %5$s     %1$s \033[0;31m%6$,d\033[0m  %1$s   \n",line,redClr,reset,"York","York",pop2);
+        System.out.println(boader);
+    }   
+    
 }
